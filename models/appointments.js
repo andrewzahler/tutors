bb10e146ada7c58893302ce1681caecc24930607
[Andrew Zahler] <[andrew.zahler@gmail.com]>
2017-05-26 08:48:50 -0400

    creating file structure

---
+++ models/appointments.js
@@ -0,0 +1,6 @@
+module.exports = function(sequelize, DataTypes) {
+
+	
+	// code for Model here
+
+};
---
+++ models/tutors.js
@@ -0,0 +1,5 @@
+module.exports = function(sequelize, DataTypes) {
+
+	// code for Model here
+
+};
