2d098e00fb1f9cb02126e3b69404549cbb820127
Diaeddin Motan <Diaeddin@Diaeddins-Air.home>
2017-05-30 21:49:07 -0400

    Updateing students page

--- controllers/tutors_controllers.js
+++ controllers/tutors_controllers.js
@@ -6,8 +6,26 @@ var db = require("../models");
 
 
 router.get('/index', function(req, res, next){
+	// db.Student.findAll({
+
+ //    }).then(function(dbStudent) {
+ //      // console.log(dbBurger);
+ //      // var burgers = dbBurger[0].dataValues;
+ //      // console.log(burgers);
+ //      var hbsObject = {
+ //      students: dbStudent
+ //    };
+ //    console.log(hbsObject);
+ //    res.render("index", hbsObject);
+ //      // res.json(dbBurger);
+ //    });
 	res.render('index');
-})
+});
+
+router.get('/students', function(req, res, next){
+
+	res.render('students');
+});
 
 
 router.get('/register', function(req, res, next){
--- public/assets/js/tutors_js.js
+++ public/assets/js/tutors_js.js
@@ -1,4 +1,4 @@
-$('body').vegas({
+$('.body').vegas({
         slides: [{
             src: '/assets/images/slide1.jpg'
         }, {
@@ -9,3 +9,8 @@ $('body').vegas({
             src: '/assets/images/slide4.jpg'
         }]
     });
+$('select').material_select();
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
