b4e48609fd44a81a28c937fae63f8a1d8433ef37
[Andrew Zahler] <[andrew.zahler@gmail.com]>
2017-06-06 12:57:33 -0400

    update to config.json

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'baseketBall',
     database: 'tutors_db'
 });
 }
